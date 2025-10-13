95e04fb6178cd19ffde308fb1d982a45ce9f48da
gunnarmein-ts <gunnar@together.science>
2025-10-13 10:39:50 -0700

    new hardware classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScorpIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScorpIntake.java
@@ -1,4 +1,54 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class ScorpIntake {
+    private DcMotor left;
+    private DcMotor right;
+    private static final double SPEED = 1.0;
+
+    ScorpIntake(HardwareMap hm, String leftName, String rightName) {
+        left = hm.get(DcMotor.class, leftName);
+        right = hm.get(DcMotor.class, rightName);
+
+        if (left != null && right != null) {
+            // no encoder, brake on zero
+            left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            // intake motors run in opposite directions
+            left.setDirection(DcMotor.Direction.FORWARD);
+            right.setDirection(DcMotor.Direction.REVERSE);
+        }
+    }
+
+    void on() {
+        if (left == null || right == null) {
+            return ;
+        }
+
+        left.setPower(SPEED);
+        right.setPower(SPEED);
+    }
+
+    void off() {
+        if (left == null || right == null) {
+            return ;
+        }
+
+        left.setPower(0);
+        right.setPower(0);
+    }
+
+    void reverse() {
+        if (left == null || right == null) {
+            return ;
+        }
+
+        left.setPower(-SPEED);
+        right.setPower(-SPEED);
+    }
 }
