7cbe1af34780e3ce5b5e9c085b75105922ae6b08
gunnarmein-ts <gunnar@together.science>
2025-10-13 11:03:15 -0700

    cannon methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScorpSorter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScorpSorter.java
@@ -5,8 +5,23 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class ScorpSorter {
     private final Servo servo;
+    private static final double POSITION_HOLD = 0.0;
+    private static final double POSITION_LEFT = 1.0;
+    private static final double POSITION_RIGHT = -1.0;
 
     ScorpSorter(HardwareMap hm, String servoName){
         servo = hm.get(Servo.class, servoName);
+    }
+
+    void hold() {
+        servo.setPosition(POSITION_HOLD);
+    }
+
+    void sortLeft() {
+        servo.setPosition(POSITION_LEFT);
+    }
+
+    void sortRight() {
+        servo.setPosition(POSITION_RIGHT);
     }
 }
