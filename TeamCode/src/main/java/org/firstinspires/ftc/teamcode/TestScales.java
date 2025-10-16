bbdcd9d33dc8fb6d36ced7f8729b40026442a9cb
gunnarmein-ts <gunnar@together.science>
2025-10-16 15:19:10 -0700

    Add

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestScales.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestScales.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+@Autonomous(name = "GMTestAuto-2", group = "Sensor")
 public class TestScales extends BaseAuto{
     @Override
     protected void auto(){
